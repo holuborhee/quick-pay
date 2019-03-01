5afd7fac154159d58f6648313998770d897e2ba2
John Olubori David <daveholuborhee@gmail.com>
2019-03-01 13:22:54 +0100

    Finish UI for New Tranfer

---
+++ src/components/TransfersNew.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-form inline class="d-flex justify-content-between mb-5">
+    <b-form-select
+      class="mb-2 mr-sm-2 mb-sm-0"
+      v-model="transfer.recipient"
+      :options="{ '1': 'One', '2': 'Two', '3': 'Three' }"
+    >
+      <option slot="first" :value="null">Choose...</option>
+    </b-form-select>
+
+    <b-form-input
+      type="number"
+      v-model="transfer.amount"
+      :min="1"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      required
+      :disabled="!transfer.recipient"
+      placeholder="Enter amount to transfer"
+    />
+
+    <b-button variant="danger">Remove</b-button>
+  </b-form>
+</template>
+
+<script>
+export default {
+  props: {
+    transfer: Object,
+  },
+  watch: {
+    'transfer.recipient': function (newVal, oldVal) { // eslint-disable-line func-names
+      if (newVal && !oldVal) this.$emit('filled');
+    },
+  },
+};
+</script>
--- src/views/TransfersNew.vue
+++ src/views/TransfersNew.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <h1>Initiate a New transfer</h1>
-  </div>
+  <b-form>
+    <NewTransfer
+      @filled="addNewTransferRow"
+      v-for="transfer in transfers" :transfer="transfer"
+      :key="transfer.id"
+    />
+
+    <b-button
+      block type="submit"
+      variant="primary"
+    >
+      Perform Transfer
+    </b-button>
+  </b-form>
 </template>
+
+<script>
+import NewTransfer from '@/components/TransfersNew.vue';
+
+export default {
+  components: {
+    NewTransfer,
+  },
+  data() {
+    return {
+      transfers: [],
+      count: 0,
+    };
+  },
+  methods: {
+    addNewTransferRow() {
+      this.count += 1;
+      this.transfers.push({ id: this.count, recipient: null, amount: null });
+    },
+  },
+  mounted() {
+    this.addNewTransferRow();
+  },
+};
+</script>
